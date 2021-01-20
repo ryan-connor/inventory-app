aafd3c97fdef30abc335f6cad922c0a02f629b7c
Connor <ryanconnor786@gmail.com>
2021-01-19 17:22:45 -0700

    updated routes for home page and hat list page and added ejs templates

--- controllers/hatController.js
+++ controllers/hatController.js
@@ -1,14 +1,67 @@
-let hat = require('../models/hat');
+let Hat = require('../models/hat');
+let HatInstance = require('../models/hatInstance');
+let Category = require('../models/category');
+
 
 
 //display index/home page
 exports.index = function (req, res) {
-    res.send('Home index page not implemnted yet');
+
+let results = {};
+
+    let hat_count = async function() {
+       await Hat.countDocuments({}, function(err, count) {
+            if (err) {
+                console.log(err);
+                return err;
+            }
+            results.hat_count= count;
+        }).exec();
+    };
+
+   let hatInstance_count = async function () {
+      await HatInstance.countDocuments({}, function (err,count) {
+           if (err) {
+               console.log(err);
+               return err;
+           }
+           results.hatInstance_count= count;
+       }).exec();
+   };
+
+   let category_count = async function () {
+     await Category.countDocuments({}, function (err,count) {
+        if (err) {
+            console.log(err);
+            return err;
+        }
+        results.category_count= count;
+    }).exec();
+};
+
+//function to call all individual counts and then render them
+let getCounts = async function () {
+    //call all functions
+    await Promise.allSettled([hat_count(), hatInstance_count(), category_count()]);
+    // console.log('results:', results);
+    res.render('index', {title: 'Hat Store Home', data: results});
+    //note that still need to add error handling, currently error just gets returned to the results object
+};
+    getCounts();  
 };
 
 //display list of all hats
-exports.hat_list = function (req, res) {
-    res.send('Hat List is not implemented yet')
+exports.hat_list = function (req, res, next) {
+
+    Hat.find({}, 'name price category').populate('category').exec( function (err, list_hats) {
+        if(err) {
+            return next(err);
+        }
+        res.render('hat_list', {title: 'Hat List', hat_list: list_hats});
+    });
+
+
+    // res.send('Hat List is not implemented yet')
 }
 
 //display specific hat page
